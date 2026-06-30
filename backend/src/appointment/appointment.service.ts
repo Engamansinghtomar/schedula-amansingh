5fa3d8e6d24c90b378f7299e6482664d7846edf4
Aman Singh Tomar <at097453@gmail.com>
2026-06-30 21:59:14 +0530

    feat : implemenet appoointmnet booking window validation

--- backend/src/appointment/appointment.service.ts
+++ backend/src/appointment/appointment.service.ts
@@ -142,6 +142,55 @@ export class AppointmentService {
     const dayOfWeek =
       days[requestedDate.getDay()];
 
+    const recurringAvailability =
+      await this.recurringRepository.findOne({
+        where: {
+          doctorProfile: {
+            id: doctorId,
+          },
+          dayOfWeek,
+        },
+        relations: {
+          doctorProfile: true,
+        },
+      });
+
+    if (!recurringAvailability) {
+      throw new NotFoundException(
+        'Doctor unavailable today',
+      );
+    }
+
+    const bookingOpenTime =
+      new Date(`${date}T${recurringAvailability.startTime}:00`);
+
+    bookingOpenTime.setHours(
+      bookingOpenTime.getHours() - 30,
+    );
+
+    const bookingCloseTime =
+      new Date(`${date}T${recurringAvailability.endTime}:00`);
+
+    bookingCloseTime.setHours(
+      bookingCloseTime.getHours() - 15,
+    );
+
+    const currentTime =
+      new Date();
+
+    
+    if (currentTime < bookingOpenTime) {
+      throw new BadRequestException(
+        'Booking window has not opened yet',
+      );
+    }
+
+    if (currentTime > bookingCloseTime) {
+      throw new BadRequestException(
+        'Booking window has closed',
+      );
+    }
+
     let slotExists = false;
 
     const customAvailability =
